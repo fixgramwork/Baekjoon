38c238511d75fdb9297d19db8b7d3082a89f536f
Haeyul <154101599+fixgramwork@users.noreply.github.com>
2025-10-11 23:59:50 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int MenOfPassion(int A[], int n) {
+    int i = n / 2;  // floor(n / 2)
+    return A[i];
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << 1 << '\n'; // 코드1의 수행 횟수
+    cout << 0 << '\n'; // 다항식의 최고차항 차수
+
+    return 0;
+}
