734a4b381c610e362a9650264ef233d2c3ec2d8a
Haeyul <154101599+fixgramwork@users.noreply.github.com>
2025-10-21 08:28:16 +0900

    [Silver III] Title: 수열, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n,k, temp, ret = -100001, a[100000];
+int main() {
+  ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+  cin >> n >> k;
+  for(int i = 1; i<=n;i++){
+    cin >> temp; a[i] = a[i-1] + temp;
+  }
+
+  for(int i = k; i<=n;i++) ret = max(ret, a[i] - a[i-k]);
+  cout << ret << "\n";
+}
